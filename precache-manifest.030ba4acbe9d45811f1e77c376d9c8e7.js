800d484c7fadd61b67243030d8492fe7b4d7c8f0
Ilya <ilyaphotoshop17@gmail.com>
2020-04-14 00:49:36 +0300

    Updates

---
+++ precache-manifest.030ba4acbe9d45811f1e77c376d9c8e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076e347560ca9636669c90c4c520a72a",
+    "url": "/React_WeatherApp/index.html"
+  },
+  {
+    "revision": "b2dee940ecca869558a9",
+    "url": "/React_WeatherApp/static/js/2.54364813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_WeatherApp/static/js/2.54364813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34274db6de7872201499",
+    "url": "/React_WeatherApp/static/js/main.13caf9e8.chunk.js"
+  },
+  {
+    "revision": "5bd4dd689417d854e011",
+    "url": "/React_WeatherApp/static/js/runtime-main.775e4cc1.js"
+  }
+]);
