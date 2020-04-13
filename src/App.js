27e7e8e6e2150f56085e21f4eedb90b6b89c653a
Ilya <ilyaphotoshop17@gmail.com>
2020-04-14 01:36:05 +0300

    [add] create fetch meth, and new comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 
+//import components
+import Titles from "./components/elements/Titles";
+import Form from "./components/elements/Form";
+import Weather from "./components/elements/Weather";
+import { API_KEY } from "./components/config/config";
+
+
+
 export class App extends Component {
+
+    getWeather = async (e) => {
+        e.preventDefault()
+        const city = e.target.elements.city.value
+        const country = e.target.elements.country.value
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+        const data = api_call.json()
+        console.log(data)
+    }
+
     render() {
         return(
             <div className='App'>
-                <h1>Test name </h1>
+                <Titles />
+                <Form getWeather={this.getWeather}/>
+                <Weather />
             </div>
         )
     }
---
+++ src/components/config/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = `0ecd80333f3fc44ff0ee6bf7087b756c`
+
+export const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=Kiev,Ukraine&appid=0ecd80333f3fc44ff0ee6bf7087b756c&units=metric`
