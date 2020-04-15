2302dabeecdbcffd874560a049846f60979d5caa
Ilya <ilyaphotoshop17@gmail.com>
2020-04-15 16:51:49 +0300

    [add] added new properties to weather components

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ import Titles from "./components/elements/Titles";
 import Form from "./components/elements/Form";
 import Weather from "./components/elements/Weather";
 import { API_KEY } from "./components/config/config";
-
+import Axios from "axios";
 
 
 export class App extends Component {
+
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: false,
+
+    }
 
     getWeather = async (e) => {
         e.preventDefault()
         const city = e.target.elements.city.value
         const country = e.target.elements.country.value
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
-        const data = api_call.json()
+        const api_call = await Axios(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`)
+        const data = await api_call.data
         console.log(data)
+        this.setState({
+            temperature: data.main.temp,
+            city: data.name,
+            country: data.sys.country,
+            humidity: data.main.humidity,
+            description: data.weather[0].description,
+            error: ''
+        })
     }
 
     render() {
@@ -24,7 +42,14 @@ export class App extends Component {
             <div className='App'>
                 <Titles />
                 <Form getWeather={this.getWeather}/>
-                <Weather />
+                <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                />
             </div>
         )
     }
